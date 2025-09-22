6509f3c03d4f3e03f6701d2f31462a3995b03ae8
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-22 15:56:11 -0700

    k

--- javascript.js
+++ javascript.js
@@ -178,6 +178,16 @@ function hour4() {
 }
 
 function hour5() {
-    
+    let message;
+
+    if (login == 'Employee') {
+        message = 'Hello';
+    } else if (login == 'Director') {
+        message = 'Greetings';
+    } else if (login == '') {
+        message = 'No login';
+    } else {
+        message = '';
+    }
 }
 
