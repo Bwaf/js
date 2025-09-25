b234323984798328629e78f4118e916f66c14610
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-24 18:49:43 -0700

    j

--- javascript.js
+++ javascript.js
@@ -308,6 +308,21 @@ function hour7() {
 
     alert('Done!');
 
+    let a = 2 + 2;
+
+    switch (a) {
+        case 3:
+            alert('Too small');
+            break;
+        case 4:
+            alert('Exactly!');
+            break;
+        case 5:
+            alert('Too big');
+            break;
+        default:
+            alert("I don't know such values");
+    }
 }
 
 let sum = 0;
