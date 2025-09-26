86a4b7f8faa8a2e6e25b5b6cc13bbc418941ce4d
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-25 23:22:21 -0700

    more funny

--- objects.js
+++ objects.js
@@ -1,7 +1,10 @@
 let user = new Object(); //object constructer syntax
 let user = {}; //"object literal" syntax
 
-let user = {
-    name: "John",
-    age: 30
+let user = {    //an object
+    name: "John",   //by key "name" store value "John"
+    age: 30     //by key "age" store value "30"
 };
+
+alert(user.name);
+alert(user.age);
