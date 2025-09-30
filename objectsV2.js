1a522bae7d2caf046a35ab3357967477c5538b75
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-29 17:48:22 -0700

    k

--- objectsV2.js
+++ objectsV2.js
@@ -3,30 +3,58 @@
 
 // these objects do the same
 
-user = {
-    sayHi: function () {
-        alert("Hello");
-    }
-};
+// user = {
+//     sayHi: function () {
+//         alert("Hello");
+//     }
+// };
 
-// method shorthand looks better, right?
-user = {
-    sayHi() { // same as "sayHi: function(){...}"
-        alert("Hello");
-    }
-};
+// // method shorthand looks better, right?
+// user = {
+//     sayHi() { // same as "sayHi: function(){...}"
+//         alert("Hello");
+//     }
+// };
 
-let user = {
-    name: "John",
-    age: 30,
+// let user = {
+//     name: "John",
+//     age: 30,
 
-    sayHi() {
-        alert(user.name); // "user" instead of "this"
-    }
+//     sayHi() {
+//         alert(user.name); // "user" instead of "this"
+//     }
 
-};
+// };
 
-let admin = user;
-user = null; // overwrite to make things obvious
+// let admin = user;
+// user = null; // overwrite to make things obvious
 
-admin.sayHi(); // TypeError: Cannot read property 'name' of null
+// admin.sayHi(); // TypeError: Cannot read property 'name' of null
+
+//if used this.name instead of user.name inside of the alert, the code wouldnt work
+
+let user = { name: "John" };
+let admin = { name: "Admin" };
+
+function sayHi() {
+    alert(this.name);
+}
+
+// use the same function in two objects
+user.f = sayHi;
+admin.f = sayHi;
+
+// these calls have different this
+// "this" inside the function is the object "before the dot"
+user.f(); // John  (this == user)
+admin.f(); // Admin  (this == admin)
+
+admin['f'](); // Admin (dot or square brackets access the method – doesn't matter)
+
+
+
+
+
+
+
+
