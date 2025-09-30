286e6b227d8487b67300d1ed199fb27dcb9675f8
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-29 17:41:30 -0700

    k

---
+++ objectsV2.js
@@ -0,0 +1,24 @@
+// these objects do the same
+
+user = {
+    sayHi: function () {
+        alert("Hello");
+    }
+};
+
+// method shorthand looks better, right?
+user = {
+    sayHi() { // same as "sayHi: function(){...}"
+        alert("Hello");
+    }
+};
+
+let user = {
+    name: "John",
+    age: 30,
+
+    sayHi() {
+        alert(user.name); // "user" instead of "this"
+    }
+
+};
