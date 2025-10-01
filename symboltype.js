f10a015ebc687ea49c1fe49e82b539701e4696bd
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-30 20:41:50 -0700

    d

--- symboltype.js
+++ symboltype.js
@@ -7,4 +7,11 @@ let id2 = Symbol("id");
 
 alert(id1 == id2); // false
 alert(id.toString()); //symbol ID now works
-alert(id.description);
+alert(id.description); // id
+
+let user = { //belongs to another code
+    name : "John"
+};
+user[id] = 1;
+alert(user[id]);//can access the data using symbol as the key
+
