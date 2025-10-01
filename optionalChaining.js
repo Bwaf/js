63168a762238d23f61443df2c0f34aa21a41cc1a
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-30 20:31:32 -0700

    m

--- optionalChaining.js
+++ optionalChaining.js
@@ -4,7 +4,8 @@ let user = {}; //user without property
 alert(user.address.street); // causes an error
 
 // document.querySelector('.elem') is null if there's no element
-let html = document.querySelector('.elem').innerHTML;// error if it's null
+// let html = document.querySelector('.elem').innerHTML;// error if it's null
 
 alert(user.address ? user.address.street : undefined);
 
+let html = document.querySelector('.elem') ? document.querySelector('.elem').innerHTML : null;
