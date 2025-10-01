9411eb4c0ae7d68177ef2ae03ce728079ba24e78
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-30 21:29:51 -0700

    n

--- objPrimConversion.js
+++ objPrimConversion.js
@@ -8,3 +8,21 @@ let n = +obj;
 let delta = date1 - date2;
 
 let greater = user1 > user2;
+
+let total = obj1 + obj2;
+
+let user = {
+    name: "john",
+    money: 1000,
+
+    toString() {
+        return `{name: "${this.name}"}`
+    },
+
+    valueOf() {
+        return this.money;
+    }
+};
+alert(user);
+alert(+user);
+alert(user + 500);
