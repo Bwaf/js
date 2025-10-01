887f5ea2ee4b3a230477656b70c2136f09474213
Fish <115324554+Bwaf@users.noreply.github.com>
2025-09-30 20:58:54 -0700

    j

--- symboltype.js
+++ symboltype.js
@@ -11,7 +11,18 @@ alert(id.description); // id
 
 let user = { //belongs to another code
     name : "John"
+    [id]: 123
 };
 user[id] = 1;
 alert(user[id]);//can access the data using symbol as the key
 
+user[id] = "Their id value";
+
+//script uses "id" property 
+user.id = "Our id value";
+
+user.id = "Their value";
+
+for (let key user) alert(key);//name age (no symbol)
+
+alert("Direct: " + user[id]); //Direct: 123
