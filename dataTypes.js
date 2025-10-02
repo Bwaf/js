5a7f38111c6a6ba48bfebb70a360f07906800ba1
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-01 18:13:20 -0700

    t

--- dataTypes.js
+++ dataTypes.js
@@ -35,8 +35,12 @@ str.test = 5;
 
 alert(str.test);
 
-let billion = 1000000000;
+// let billion = 1000000000;
 
 // We also can use underscore _ as the separator:
 
 // let billion = 1_000_000_000;
+
+let billion = 1e9;  // 1 billion, literally: 1 and 9 zeroes
+
+alert( 7.3e9 );  // 7.3 billions (same as 7300000000 or 7_300_000_000)
