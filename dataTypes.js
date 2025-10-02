3652af34026aa46eca1c8502abb257206e32fa5e
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-01 18:03:43 -0700

    b

--- dataTypes.js
+++ dataTypes.js
@@ -24,3 +24,7 @@ let zero = new Number(0);
 if (zero) { // zero is true, because it's an object
     alert("zero is truthy!?!");
 }
+
+let num = Number("123"); // convert a string to number
+
+alert(null.test); // error
