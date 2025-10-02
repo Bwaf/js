42b7bca81f42b4a29d0c213ce5abc23a10455be9
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-01 18:38:59 -0700

    m

--- dataTypes.js
+++ dataTypes.js
@@ -155,3 +155,15 @@ function sumSalaries(department) {
 }
 
 alert(sumSalaries(company)); // 7700
+
+function showName(firstName, lastName, ...titles) {
+    alert(firstName + ' ' + lastName); // Julius Caesar
+
+    // the rest go into titles array
+    // i.e. titles = ["Consul", "Imperator"]
+    alert(titles[0]); // Consul
+    alert(titles[1]); // Imperator
+    alert(titles.length); // 2
+}
+
+showName("Julius", "Caesar", "Consul", "Imperator");
