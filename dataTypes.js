bd760656d048927b2ccbeb902d8945ee4009806b
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-01 18:15:52 -0700

    b

--- dataTypes.js
+++ dataTypes.js
@@ -47,3 +47,14 @@ alert(7.3e9);  // 7.3 billions (same as 7300000000 or 7_300_000_000)
 
 1e3 === 1 * 1000; // e3 means *1000
 1.23e6 === 1.23 * 1000000; // e6 means *1000000
+
+let mсs = 0.000001;
+
+// -3 divides by 1 with 3 zeroes
+1e-3 === 1 / 1000; // 0.001
+
+// -6 divides by 1 with 6 zeroes
+1.23e-6 === 1.23 / 1000000; // 0.00000123
+
+// an example with a bigger number
+1234e-2 === 1234 / 100; // 12.34, decimal point moves 2 times
