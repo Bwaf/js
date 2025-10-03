6fa10030c4b6b66244306e9837e19f6966a35108
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-02 17:38:12 -0700

    v

--- classes.js
+++ classes.js
@@ -7,18 +7,44 @@
 //   ...
 // }
 
+// class User {
+
+//     constructor(name) {
+//         this.name = name;
+//     }
+
+//     sayHi() {
+//         alert(this.name);
+//     }
+
+// }
+
+// // Usage:
+// let user = new User("John");
+// user.sayHi();
+
 class User {
 
     constructor(name) {
+        // invokes the setter
         this.name = name;
     }
 
-    sayHi() {
-        alert(this.name);
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (value.length < 4) {
+            alert("Name is too short.");
+            return;
+        }
+        this._name = value;
     }
 
 }
 
-// Usage:
 let user = new User("John");
-user.sayHi();
+alert(user.name); // John
+
+user = new User(""); // Name is too short.
