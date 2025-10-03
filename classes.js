27641aeeb540a074a04bd1cbbe08f30e04448bf0
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-02 17:44:55 -0700

    nn

--- classes.js
+++ classes.js
@@ -48,3 +48,17 @@ let user = new User("John");
 alert(user.name); // John
 
 user = new User(""); // Name is too short.
+
+class Button {
+    constructor(value) {
+        this.value = value;
+    }
+
+    click() {
+        alert(this.value);
+    }
+}
+
+let button = new Button("hello");
+
+setTimeout(button.click, 1000); // undefined
