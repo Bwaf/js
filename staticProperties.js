7262bc122913c74299d5138a94e0fc140d3d1c25
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-22 17:57:47 -0700

    jfk

--- staticProperties.js
+++ staticProperties.js
@@ -148,3 +148,29 @@ alert(XMLHttpRequest.prototype[Symbol.toStringTag]); // XMLHttpRequest
 
 alert({}.toString.call(window)); // [object Window]
 alert({}.toString.call(new XMLHttpRequest())); // [object XMLHttpRequest]
+
+
+//Mixins
+
+// mixin
+let sayHiMixin = {
+    sayHi() {
+        alert(`Hello ${this.name}`);
+    },
+    sayBye() {
+        alert(`Bye ${this.name}`);
+    }
+};
+
+// usage:
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+// copy the methods
+Object.assign(User.prototype, sayHiMixin);
+
+// now User can say hi
+new User("Dude").sayHi(); // Hello Dude!
