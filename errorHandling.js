a23aa01a5f80cd80abcf7f1a7c9de671ba7011ee
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-23 15:04:23 -0700

    kkkkm

---
+++ errorHandling.js
@@ -0,0 +1,10 @@
+//try catch syntax
+try {
+    //code
+    alert('Start of try runs');  // (1) <--
+    //no errors
+    alert('End of try runs');   // (2) <--
+} catch (err) {
+    //error handling
+    alert('Catch is ignored, because there are no errors'); // (3)
+}
