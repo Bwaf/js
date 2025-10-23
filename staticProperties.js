6eb6728ecb88cfb187aae55d755bca444bafb70a
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-22 17:21:18 -0700

    jjnjnnj

--- staticProperties.js
+++ staticProperties.js
@@ -76,3 +76,29 @@ alert(Rabbit.planet); // Earth
 
 
 // private and protected properites methods
+
+class CoffeeMachine {
+    _waterAmount = 0;
+
+    set waterAmount(value) {
+        if (value < 0) {
+            value = 0;
+        }
+        this._waterAmount = value;
+    }
+
+    get waterAmount() {
+        return this._waterAmount;
+    }
+
+    constructor(power) {
+        this._power = power;
+    }
+
+}
+
+// create the coffee machine
+let coffeeMachine = new CoffeeMachine(100);
+
+// add water
+coffeeMachine.waterAmount = -10; // _waterAmount will become 0, not -10
