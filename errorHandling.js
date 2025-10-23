61ba25f139b2cf4f267d9ccdea447b20d9157d26
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-23 15:24:00 -0700

    asdfasdf

--- errorHandling.js
+++ errorHandling.js
@@ -18,3 +18,25 @@ try {
     // The error is converted to string as "name: message"
     alert(err); // ReferenceError: lalala is not defined
 }
+
+let json = '{"name":"John", "age": 30}'; // data from the server
+
+let user = JSON.parse(json); // convert the text representation to JS object
+
+// now user is an object with properties from the string
+alert(user.name); // John
+alert(user.age);  // 30
+
+let json = "{ bad json }";
+
+try {
+
+    let user = JSON.parse(json); // <-- when an error occurs...
+    alert(user.name); // doesn't work
+
+} catch (err) {
+    // ...the execution jumps here
+    alert("Our apologies, the data has errors, we'll try to request it one more time.");
+    alert(err.name);
+    alert(err.message);
+}
