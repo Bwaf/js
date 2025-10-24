50e1d189d9cefc773f23535b955e7fa0d029b07e
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-24 13:54:53 -0700

    nm

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -3,3 +3,16 @@ async function f() {
 }
 
 f().then(alert); // 1
+
+async function f() {
+
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("done!"), 1000)
+    });
+
+    let result = await promise; // wait until the promise resolves (*)
+
+    alert(result); // "done!"
+}
+
+f();
