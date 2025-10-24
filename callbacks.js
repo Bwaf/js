5e850eddca8d506e20d3de7cbe8e25fb5e61aae9
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-23 18:01:43 -0700

    hghg

--- callbacks.js
+++ callbacks.js
@@ -82,3 +82,27 @@ promise.then(
 );
 
 promise.then(script => alert('Another handler...'));
+
+
+//promise chaining
+
+new Promise(function (resolve, reject) {
+
+    setTimeout(() => resolve(1), 1000); // (*)
+
+}).then(function (result) { // (**)
+
+    alert(result); // 1
+    return result * 2;
+
+}).then(function (result) { // (***)
+
+    alert(result); // 2
+    return result * 2;
+
+}).then(function (result) {
+
+    alert(result); // 4
+    return result * 2;
+
+});
