26573281eaecb006888f51a47d9c12bff58886a5
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-23 17:43:21 -0700

    m

--- callbacks.js
+++ callbacks.js
@@ -1,10 +1,11 @@
-function loadScript(src) {
-  // creates a <script> tag and append it to the page
-  // this causes the script with given src to start loading and run when complete
-  let script = document.createElement('script');
-  script.src = src;
-  document.head.append(script);
+function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = () => callback(script);
+    document.head.append(script);
 }
 
-// load and execute the script at the given path
-loadScript('/my/script.js');
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+    alert(`Cool, the script ${script.src} is loaded`);
+    alert(_); // _ is a function declared in the loaded script
+});
