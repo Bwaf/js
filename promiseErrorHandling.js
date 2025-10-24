a41a32510e0224439b48f5fdd3692e11057ad368
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-23 18:21:19 -0700

    njkhyghjvn m

--- promiseErrorHandling.js
+++ promiseErrorHandling.js
@@ -60,3 +60,18 @@ Promise.all([
     new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
     new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
 ]).then(alert); // 1,2,3 when promises are ready: each promise contributes an array member
+
+let urls = [
+    'https://api.github.com/users/iliakan',
+    'https://api.github.com/users/remy',
+    'https://api.github.com/users/jeresig'
+];
+
+// map every url to the promise of the fetch
+let requests = urls.map(url => fetch(url));
+
+// Promise.all waits until all jobs are resolved
+Promise.all(requests)
+    .then(responses => responses.forEach(
+        response => alert(`${response.url}: ${response.status}`)
+    ));
