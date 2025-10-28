d2228107c1beae5f5b627abe78742dd3810edf5e
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-28 16:33:11 -0700

    balls

--- generators.js
+++ generators.js
@@ -3,3 +3,7 @@ function* generateSequence() {
     yield 2;
     return 3;
 }
+
+// "generator function" creates "generator object"
+let generator = generateSequence();
+alert(generator); // [object Generator]
