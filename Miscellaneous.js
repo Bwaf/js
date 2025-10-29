e64f753f6b2b186edc0c38ba5a520dba13f2af42
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-29 14:40:39 -0700

    jlaksjljlksdfjlkasjdflkjjfkjljkjasdkkkjfunction jkfdlkajsdfljcodingadjflakjsdf

--- Miscellaneous.js
+++ Miscellaneous.js
@@ -7,3 +7,18 @@ alert(target.test); // 5, the property appeared in target!
 alert(proxy.test); // 5, we can read it from proxy too (2)
 
 for (let key in proxy) alert(key); // test, iteration works (3)
+
+let numbers = [0, 1, 2];
+
+numbers = new Proxy(numbers, {
+    get(target, prop) {
+        if (prop in target) {
+            return target[prop];
+        } else {
+            return 0; // default value
+        }
+    }
+});
+
+alert(numbers[1]); // 1
+alert(numbers[123]); // 0 (no such item)
