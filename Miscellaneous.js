af1e212f6b6d249a4e03982d4db131967ef6fe9b
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-29 14:38:19 -0700

    hfasdjklfaj;sdflkajdasdflkj

---
+++ Miscellaneous.js
@@ -0,0 +1,9 @@
+let target = {};
+let proxy = new Proxy(target, {}); // empty handler
+
+proxy.test = 5; // writing to proxy (1)
+alert(target.test); // 5, the property appeared in target!
+
+alert(proxy.test); // 5, we can read it from proxy too (2)
+
+for (let key in proxy) alert(key); // test, iteration works (3)
