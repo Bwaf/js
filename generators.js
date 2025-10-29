31eaabfaa7561e5964d503a03671d6cfe64e5e8a
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-28 17:17:24 -0700

    giugyuggh

--- generators.js
+++ generators.js
@@ -25,3 +25,21 @@ let range = {
 
 // iteration over range returns numbers from range.from to range.to
 alert([...range]); // 1,2,3,4,5
+
+function* gen() {
+    let ask1 = yield "2 + 2 = ?";
+
+    alert(ask1); // 4
+
+    let ask2 = yield "3 * 3 = ?"
+
+    alert(ask2); // 9
+}
+
+let generator = gen();
+
+alert(generator.next().value); // "2 + 2 = ?"
+
+alert(generator.next(4).value); // "3 * 3 = ?"
+
+alert(generator.next(9).done); // true
