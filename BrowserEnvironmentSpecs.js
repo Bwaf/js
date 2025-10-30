34226e40cdd39a8946185979cbe5fee21cfb3ea5
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-30 15:54:48 -0700

    n vgcfghvjbn

--- BrowserEnvironmentSpecs.js
+++ BrowserEnvironmentSpecs.js
@@ -6,3 +6,9 @@ function sayHi() {
 window.sayHi();
 
 alert(window.innerHeight); // inner window height
+
+// change the background color to red
+document.body.style.background = "red";
+
+// change it back after 1 second
+setTimeout(() => document.body.style.background = "", 1000);
