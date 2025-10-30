0723ad8bb81656dc0b8a01e24b8fc3b40692d0f1
Fish <115324554+Bwaf@users.noreply.github.com>
2025-10-30 15:47:21 -0700

    bhvgb

--- WeakRefAndFinalizationegistry.js
+++ WeakRefAndFinalizationegistry.js
@@ -8,3 +8,27 @@ let admin = user;
 user = null;
 
 // the object is still reachable through the admin variable
+
+
+function fetchImg() {
+    // abstract function for downloading images...
+}
+
+function weakRefCache(fetchImg) { // (1)
+    const imgCache = new Map(); // (2)
+
+    return (imgName) => { // (3)
+        const cachedImg = imgCache.get(imgName); // (4)
+
+        if (cachedImg?.deref()) { // (5)
+            return cachedImg?.deref();
+        }
+
+        const newImg = fetchImg(imgName); // (6)
+        imgCache.set(imgName, new WeakRef(newImg)); // (7)
+
+        return newImg;
+    };
+}
+
+const getCachedImg = weakRefCache(fetchImg);
